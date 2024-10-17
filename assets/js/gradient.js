680951cf35e8baea832b4fb8d3edfd05f845e47e
Marion Jolas <marion.jolas@noesya.coop>
2024-10-17 11:29:57 +0200

    gradient

--- assets/js/gradient.js
+++ assets/js/gradient.js
@@ -1,11 +1,13 @@
-document.addEventListener('mousemove', function(event) {
+document.addEventListener('mousemove', function (event) {
     var { innerWidth: width, innerHeight: height } = window;
-    var x = event.clientX / width;
-    var y = event.clientY / height;
+    var x = event.pageX / width;
+    var y = event.pageY / height;
 
+    // Ajouter de la transparence à la couleur rouge (#FF4AC3 devient rgba(255, 74, 195, 0.7))
     var gradient = `
-        radial-gradient(circle at ${(x * 100)}% ${(y * 100)}%, #FF4AC3 10%, #FE5007 20%, #B0BEBF 100%)
+        radial-gradient(circle at ${event.pageX}px ${event.pageY}px, rgba(255, 74, 195, 1) 0%, rgba(254, 80, 7, 0.5) 2%, transparent 30%)
     `;
 
+    // Appliquer le dégradé comme arrière-plan du body
     document.querySelector('body').style.background = gradient;
-})
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,3 @@
 import './vendors/lightbox';
 import './theme/';
+import './gradient';
