95d3796c9588b0961a12e854ca2ed6bf84cb0899
Marion Jolas <marion.jolas@noesya.coop>
2024-10-17 13:42:33 +0200

    adaptation phone

--- assets/js/gradient.js
+++ assets/js/gradient.js
@@ -1,10 +1,18 @@
-document.addEventListener('mousemove', function (event) {
+function updateGradient(x, y) {
     var { innerWidth: width, innerHeight: height } = window;
-    var x = event.pageX / width;
-    var y = event.pageY / height;
+
     var gradient = `
-        radial-gradient(circle at ${event.pageX}px ${event.pageY}px, rgba(255, 74, 195, 1) 0%, rgba(254, 80, 7, 0.7) 1%, transparent %)
-    `;
+      radial-gradient(circle at ${x}px ${y}px, rgba(255, 74, 195, 1) 0%, rgba(254, 80, 7, 0.7) 2%, transparent 30%)
+  `;
 
     document.querySelector('body').style.background = gradient;
+}
+
+document.addEventListener('mousemove', function (event) {
+    updateGradient(event.pageX, event.pageY);
+});
+
+document.addEventListener('touchmove', function (event) {
+    var touch = event.touches[0];
+    updateGradient(touch.pageX, touch.pageY);
 });
